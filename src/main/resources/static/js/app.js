b562d84789912d4f69375330cca7a4db84d4cc9b
Damian McDonald <damianmcdonald@users.noreply.github.com>
2015-09-26 18:50:54 +0200

    Removes src and pom.xml prior to upgrade

--- src/main/resources/static/js/app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-/* App Module */
-
-var grandSlamSim = angular.module('grandSlamSim', ['grandSlamSim.controllers',
-                                               'grandSlamSim.services']);
